7a82279566323c1caa4a64a24cb74f204b4bb006
Aryan Barnwal <aryanbarnwal007@gmail.com>
2023-05-01 09:34:40 +0530

    Added app to settings.py

--- elevator_backend/settings.py
+++ elevator_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'elevator_backend',
+    'elevator_apis',
 ]
 
 MIDDLEWARE = [
