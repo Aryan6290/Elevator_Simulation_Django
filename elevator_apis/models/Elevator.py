a83c61452623082503b08b2ed5b989edd50b677d
Aryan Barnwal <aryanbarnwal007@gmail.com>
2023-05-01 09:33:56 +0530

    updated Elevator model

--- elevator_apis/models/Elevator.py
+++ elevator_apis/models/Elevator.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Elevator(models.Model):
     id = models.AutoField(primary_key=True)
-    current_floor = models.PositiveIntegerField(null=False, blank=False)
+    current_floor = models.IntegerField(null=False, blank=False,default=0)
     is_operational = models.BooleanField(default=True, null=False, blank=False)
-    is_selected = models.BooleanField(default=True, null=False, blank=False)
     is_moving = models.BooleanField(default=True, null=False, blank=False)
-    direction = models.IntegerField(null=False, blank=False, choices=[1, -1])
+    direction = models.IntegerField(null=False, blank=False)
     door_opened = models.BooleanField(null=False, blank=False, default=False)
+    last_floor = models.IntegerField(null=True, blank=False)
